620e2b5bde65e9a1f3262faa1878ba36b529cb4d
8a-ma <e0as0k98x001b@gmail.com>
2026-05-19 08:55:10 -0400

    refacto: modify data type

--- src/settings/settings.py
+++ src/settings/settings.py
@@ -9,7 +9,7 @@ load_dotenv(dotenv_path=None if not ROOT_ENV.exists() else ROOT_ENV)
 
 
 class Settings:
-    GRID_SIZE = getenv("GRID_SIZE", -1)
-    N_AGENTS = getenv("N_AGENTS", -1)
+    GRID_SIZE: int = int(getenv("GRID_SIZE", -1))
+    N_AGENTS: int = int(getenv("N_AGENTS", -1))
 
     ROOT_PATH = Path(__file__).resolve().parents[1]
