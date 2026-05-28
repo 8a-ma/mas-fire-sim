55b2ffadbe5944fd93487f3925aef9a3a6c1b39a
8a-ma <e0as0k98x001b@gmail.com>
2026-05-28 09:28:08 -0400

    feat: add flammable and color atributes on Forest pattern

--- src/patterns/terrain/forest.py
+++ src/patterns/terrain/forest.py
@@ -7,5 +7,7 @@ PatternRegistry.register(
     .adjacent_to('water', affinity=0.4)
     .adjacent_to('grass', affinity=0.2)
     .adjacent_to('mountain', affinity=0.1)
+    .flammable(True)
+    .color("#0e5906")
     .build()
 )
